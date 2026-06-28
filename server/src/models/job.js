763235ebb9e5451f8fe907ef8829c51e1c2d82b3
SANDEEP KUMAR <sandeepkumar@Mac.lan>
2026-06-28 20:52:26 +0530

    updated

--- server/src/models/job.js
+++ server/src/models/job.js
@@ -54,6 +54,12 @@ const jobSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
+    applications: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Application",
+      },
+    ],
   },
   {
     timestamps: true,
